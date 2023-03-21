364da3f488e6a8c1f82e87b261e5e239d64ebe34
Caelan van Eijnsbergen <caelan.van.eijnsbergen@gmail.com>
2023-03-21 18:33:43 +0100

    trying to fix

--- tests/sprintTests.test.ts
+++ tests/sprintTests.test.ts
@@ -1,10 +1,10 @@
 import { Project } from "../src/core/project";
 import { ProjectManagement } from "../src/core/projectManagement";
 import { Sprint } from "../src/core/sprint";
-import { ExportReportToPDF } from "../src/reportExportStrategy/exportReportToPDF";
+import { ExportReportToPNG } from "../src/reportExportStrategy/exportReportToPNG";
 
 test("should have sprint inside project", () => {
-	let reportExportStrategy = new ExportReportToPDF();
+	let reportExportStrategy = new ExportReportToPNG();
 	let sprint = new Sprint("Sprint1", reportExportStrategy);
 	let projectManagement = new ProjectManagement(sprint);
 	let project = new Project(projectManagement);
