269bbbdb1347e85c8acc9e4e5cd2a4dd27e13f93
Caelan van Eijnsbergen <caelan.van.eijnsbergen@gmail.com>
2023-03-21 16:03:30 +0100

    added first sprint test

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,3 @@ import { Sprint } from "./core/sprint";
 let sprint = new Sprint("Sprint1");
 let projectManagement = new ProjectManagement(sprint);
 let project = new Project(projectManagement);
-
-project.printProject();
