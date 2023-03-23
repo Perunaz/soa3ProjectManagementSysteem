61c5870b28b774065e943ab8d6b8c3751ec0690b
Caelan van Eijnsbergen <caelan.van.eijnsbergen@gmail.com>
2023-03-23 16:34:16 +0100

    fixed adapter pattern

--- src/messenger/messengerAdapter.ts
+++ src/messenger/messengerAdapter.ts
@@ -1,36 +1,15 @@
 import { MessageService } from "./messageService";
-import { MessengerInterface } from "./messengerInterface";
-import { DiscordService } from "./discordService";
-import { EmailService } from "./emailService";
-import { WhatsappService } from "./whatsappService";
 
-export class MessengerAdapter implements MessengerInterface {
+export class MessengerAdapter {
 
   private messageService: MessageService;
 
   constructor(adaptee: MessageService) {
     this.messageService = adaptee;
-    
-  }
-  notifySprintCreated(): void {
-    this.messageService.sendMessage("Sprint created");
-  }
-  notifySprintCancelled(): void {
-    this.messageService.sendMessage("Sprint cancelled");
-  }
-  notifySprintFinished(): void {
-    this.messageService.sendMessage("Sprint created");
-  }
-  notifyToDoItem(): void {
-    this.messageService.sendMessage("Sprint created");
   }
 
-  notifyDoingItem(): void {
-    this.messageService.sendMessage("Sprint created");
-  }
-
-  notifyDoneItem(): void {
-    this.messageService.sendMessage("Sprint created");
+  sendMessage(body: string): void {
+    this.messageService.sendMessage(body);
   }
 }
 
--- src/messenger/messengerInterface.ts
+++
@@ -1,9 +0,0 @@
-
-export interface MessengerInterface {
-    notifySprintCreated(): void;
-    notifySprintCancelled(): void;
-    notifySprintFinished(): void;
-    notifyToDoItem(): void;
-    notifyDoingItem(): void;
-    notifyDoneItem(): void;
-  }
