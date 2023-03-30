48407f5ade4b86da92153df461280a2345092ef5
joepdonkers <joepie.donkers@gmail.com>
2023-03-30 09:19:55 +0200

    added Singleton pattern to project

--- src/core/project.ts
+++ src/core/project.ts
@@ -1,13 +1,21 @@
 import { ProjectManagement } from "./projectManagement";
 
 export class Project {
+    private static instance: Project;
     private projectManagement: ProjectManagement;
 
-    constructor(projectManagement: ProjectManagement) { 
+    private constructor(projectManagement: ProjectManagement) { 
         this.projectManagement = projectManagement;
     }
 
-    getprojectManagement(): ProjectManagement { 
+    public static getInstance(projectManagement: ProjectManagement): Project {
+        if (!Project.instance) {
+            Project.instance = new Project(projectManagement);
+        }
+        return Project.instance;
+    }
+
+    getProjectManagement(): ProjectManagement { 
         return this.projectManagement;
     }
 }
